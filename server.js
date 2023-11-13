a9936abc9407508f8fc87f74beabc615a39e8ed4
KarthikSharma-7 <karthiksharma72003@gmail.com>
2023-11-13 19:45:14 +0530

    fix cors

--- server.js
+++ server.js
@@ -2,13 +2,20 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const http = require('http');
+const cors = require('cors');
 const { Server } = require('socket.io');
 const ACTIONS = require('./src/Actions');
-const PORT = process.env.PORT || 5000;
+
+const PORT = process.env.PORT;
 
 const server = http.createServer(app);
 const io = new Server(server);
 
+app.use(
+  cors({
+    origin: '*',
+  })
+);
 app.use(express.static('build'));
 
 app.use((req, res, next) => {
